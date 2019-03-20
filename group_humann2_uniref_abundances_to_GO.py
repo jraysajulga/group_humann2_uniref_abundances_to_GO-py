4ddd0269de5c48a37766647e7f09286471ea547a
jraysajulga <jraysajulga@gmail.com>
2019-03-20 11:18:58 -0500

    add argument types

--- group_humann2_uniref_abundances_to_GO.py
+++ group_humann2_uniref_abundances_to_GO.py
@@ -2,38 +2,36 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i",
+    parser.add_argument("-i", type = str,
                         help = "Path to file with UniRef50 gene family abundance (HUMAnN2 output)",
                         required = True)
-    parser.add_argument("-m",
+    parser.add_argument("-m", type = str,
                         help = "Path to file which will contain GO slim term"
                                 "abundances corresponding to molecular functions",
                         required = True)
-    parser.add_argument("-b",
+    parser.add_argument("-b", type = str,
                         help = "Path to file which will contain GO slim term"
                                 "abundances corresponding to biological processes",
                         required = True)
-    parser.add_argument("-c",
+    parser.add_argument("-c", type = str,
                         help = "Path to file which will contain GO slim term"
                                 "abundances corresponding to cellular components",
                         required = True)
 
-    parser.add_argument("-a",
+    parser.add_argument("-a", type = str,
                         help = "Path to basic Gene Ontology file")
-    parser.add_argument("-s",
+    parser.add_argument("-s", type = str,
                         help = "Path to basic slim Gene Ontology file")
-    parser.add_argument("-u",
+    parser.add_argument("-u", type = str,
                         help = "Path to file with HUMAnN2 correspondence"
                                 " between UniRef50 and GO")
-    parser.add_argument("-g",
+    parser.add_argument("-g", type = str,
                         help = "Path to GoaTools scripts")
-    parser.add_argument("-p",
+    parser.add_argument("-p", type = str,
                         help = "Path to HUMAnN2 scripts")
 
 
     args = parser.parse_args()
-
-    print(args.echo)
 
 
 if __name__=="__main__":
